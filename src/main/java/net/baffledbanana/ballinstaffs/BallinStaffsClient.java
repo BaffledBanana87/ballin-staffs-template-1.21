59761b4bde38e9685dad1b22daf1fc25c6528b5d
BaffledBanana87 <baffledbananastudios@gmail.com>
2024-07-25 20:28:27 -0400

    Initial commit

---
+++ src/main/java/net/baffledbanana/ballinstaffs/BallinStaffsClient.java
@@ -0,0 +1,10 @@
+package net.baffledbanana.ballinstaffs;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class BallinStaffsClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
